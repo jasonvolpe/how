001fbea9d3122d566ff9b0570cbb27d870985c63
Jason Volpe <jason.volpe@gmail.com>
2018-03-30 00:21:12 -0700

    Basic install script

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+#
+# installs how
+##################################################
+
+set -e
+
+readonly HOW="how"
+readonly INSTALL_DIR="how"
+readonly INSTALL_ROOT="/usr/local"
+readonly INSTALL_ALIAS_PATH="/usr/local/bin"
+readonly GIT_URL="https://github.com/jasonvolpe/how.git"
+
+function main() {
+	local path
+	path="${INSTALL_ROOT}/${INSTALL_DIR}"
+
+	echo "installing in ${path}..."
+
+	$(sudo git clone "${GIT_URL}" "${path}")
+
+	$(sudo ln "${path}/${HOW}" "${INSTALL_ALIAS_PATH}/${HOW}")
+}
+
+main "$@"
