fcefdf3e3f7f19625d21ecf5e2ab35078968ae60
Jason Volpe <jason.volpe@gmail.com>
2018-03-30 02:18:30 -0700

    Adds --update to refresh repo, stores install path in config

--- install.sh
+++ install.sh
@@ -10,16 +10,19 @@ readonly INSTALL_DIR="how"
 readonly INSTALL_ROOT="/usr/local"
 readonly INSTALL_ALIAS_PATH="/usr/local/bin"
 readonly GIT_URL="https://github.com/jasonvolpe/how.git"
+readonly HOW_CONFIG="${HOME}/.how"
 
 function main() {
 	local path
 	path="${INSTALL_ROOT}/${INSTALL_DIR}"
 
-	echo "installing in ${path}..."
+	echo "installing in ${path}"
 
-	$(git clone "${GIT_URL}" "${path}")
+	git clone "${GIT_URL}" "${path}"
 
-	$(ln "${path}/${HOW}" "${INSTALL_ALIAS_PATH}/${HOW}")
+	ln "${path}/${HOW}" "${INSTALL_ALIAS_PATH}/${HOW}"
+
+	echo "HOW_ROOT=\"${path}\"" > "${HOW_CONFIG}"
 }
 
 main "$@"
