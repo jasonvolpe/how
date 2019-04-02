a024ba4f78005e821c235aca843cdc4bd70d9498
Jason Volpe <jason.volpe@gmail.com>
2019-04-01 22:14:40 -0700

    More travis tweaks

--- install.sh
+++ install.sh
@@ -6,8 +6,7 @@
 set -e
 
 readonly HOW="how"
-readonly INSTALL_DIR="how"
-readonly INSTALL_ROOT="/usr/local"
+readonly INSTALL_PATH="/usr/local/how"
 readonly INSTALL_ALIAS_PATH="/usr/local/bin"
 readonly GIT_URL="https://github.com/jasonvolpe/how.git"
 readonly HOW_CONFIG="${HOME}/.how"
@@ -17,18 +16,29 @@ function main() {
 	local path
 	local alias_path
 	local repo
+	local branch
 
-	path="${INSTALL_ROOT}/${INSTALL_DIR}"
+	path="${INSTALL_PATH}"
 	alias_path="${INSTALL_ALIAS_PATH}/${HOW}"
 	repo="${1:-$GIT_URL}"
+	branch="$(git rev-parse --abbrev-ref HEAD)"
 
 	# Clone how repo
 	if [[ $(test -w "${path}") ]]; then
-		echo "Cloning ${repo} to ${path}"
-		git clone "${repo}" "${path}" || exit 1
+		echo "Cloning ${repo}:${branch} to ${path}"
+		git clone -b "${branch}" --single-branch "${repo}" "${path}" || exit 1
+	else
+		# Checkout current branch
+		echo "Checking out ${branch}"
+		pushd ${INSTALL_PATH}
+		git fetch
+		git checkout "${branch}"
+		popd
 	fi
 
 	# Create symlink
+	echo "Deleting symlink ${alias_path}"
+	rm ${alias_path}
 	if [[ $(test -w "${alias_path}") ]]; then
 		echo "Creating symlink ${alias_path}"
 		ln "${path}/${HOW}" "${alias_path}" || exit 3
