a23bb622bc93de3060af2f985a32ca34593357bb
Jason Volpe <jason.volpe@gmail.com>
2019-04-01 20:51:21 -0700

    Adds repo source override to install.sh

--- install.sh
+++ install.sh
@@ -12,17 +12,21 @@ readonly INSTALL_ALIAS_PATH="/usr/local/bin"
 readonly GIT_URL="https://github.com/jasonvolpe/how.git"
 readonly HOW_CONFIG="${HOME}/.how"
 
+# param 1: path of source repo (optional)
 function main() {
 	local path
 	local alias_path
+	local repo
+
 	path="${INSTALL_ROOT}/${INSTALL_DIR}"
 	alias_path="${INSTALL_ALIAS_PATH}/${HOW}"
+	repo="${1:-$GIT_URL}"
 
-	echo "installing in ${path}"
+	echo "Installing in ${path} from ${repo}"
 
 	# Clone how repo
 	if [[ $(test -w "${path}") ]]; then
-		git clone "${GIT_URL}" "${path}"
+		git clone "${repo}" "${path}"
 	fi
 
 	# Create symlink
